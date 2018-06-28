28d05a149a982724d4f8994e4f44948e97f9b27a
rajan sharma <rajansharma9697@gmail.com>
2018-06-28 19:55:29 +0530

    pdf for invoice generated
    pdf generated from a json for invoice

---
+++ printFunctionality/api/controllers/JsontopdfController.js
@@ -0,0 +1,315 @@
+const pdf = require('html-pdf');
+const ejs = require('ejs');
+const fs = require('fs');
+
+var data = [
+  {
+    'invoice': '1234',
+    'billed_to': {
+      'name': 'Rajan sharma',
+      'street': '2 krishna duplex',
+      'city': 'vadodara'
+    },
+    'shipped_to': {
+      'name': 'Mayur Chhapra',
+      'street': '2 Radha Bungalows',
+      'city': 'Ahemdabad'
+    },
+    'payment': 'Netbanking,SBI',
+    'order_date': '21 August,2018',
+    'transactions': [
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur pancharisht',
+        'price': '10',
+        'quantity': '4',
+        'totals': '40rs'
+
+      },
+      {
+        'item': 'Dabur honey',
+        'price': '10',
+        'quantity': '4',
+        'totals': '40rs'
+
+      },
+      {
+        'item': 'Dabur chavanprash',
+        'price': '10',
+        'quantity': '4',
+        'totals': '40rs'
+
+      },
+      {
+        'item': 'Dabur su khabar',
+        'price': '10',
+        'quantity': '4',
+        'totals': '40rs'
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },{
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },{
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      },
+
+      {
+        'item': 'Dabur toothpaste',
+        'price': '10',
+        'quantity': '4',
+        'total': '40rs'
+
+      }
+
+    ],
+    'subtotal': '200 rs',
+    'shipping': '10 rs',
+    'total': '210 rs'
+  }
+
+];
+
+const HtmltoPDF = async (req, res) => {
+  var template = fs.readFileSync('views/pages/businesscard.ejs', 'utf8');
+  var html = ejs.render(template, { data: data });
+  let htmlOptions = {
+    format: 'Letter',
+    'border': '0cm'
+  };
+
+  let downloadPath = `${__dirname.split('controllers')[0]}/download/report.pdf`;
+
+  pdf.create(html, htmlOptions).toFile(downloadPath, (error, success) => {
+
+    if (error) {
+      res.json({ data: error });
+    }
+    else {
+      //res.view('pages/businesscard',{data:data});
+      res.view('pages/businesscard', { data: data });
+    }
+  });
+};
+
+module.exports = {
+  HtmltoPDF
+};
+
+
